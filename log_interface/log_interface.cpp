a087f1ad2f421ee7bd75bda688a8381093809e79
miguelboing <miguel-boing@hotmail.com>
2022-11-21 06:42:03 -0300

    log_interface: Implementation for Request and Read Log data

--- log_interface/log_interface.cpp
+++ log_interface/log_interface.cpp
@@ -4,23 +4,113 @@
 
 #include "uart_driver.h"
 
-int main(){
-	int i = 0;
-	UartCommunication uart(BR9600, ONE_STOP_BIT, DISABLE_PARITY_BIT, EIGHT_BITS_PER_BYTE);
 
-	unsigned char msg[] = { 'H', 'e', 'l', 'l', 'o', '\r' };
+typedef struct 
+{
+    unsigned char temperature;
+    unsigned char humidity;
+}Telemetry_t;
 
-	unsigned char read_buf[256] = {0};
-	int av_bytes = 0;
+typedef struct 
+{
+    unsigned char year;
+    unsigned char month;
+    unsigned char day;
+}Date_t;
 
-	uart.write_data(msg, sizeof(msg));
+typedef struct 
+{
+    Telemetry_t telemetry;
+    unsigned char n_prev;
+    unsigned char n_success;
+    unsigned char uc_ID;
+    Date_t date;
+}Data_t;
 
-	av_bytes = uart.read_data(read_buf);
-	printf("Received bytes: %i \n", av_bytes);
-	printf("Received message: \n");
-	for(i=0;i<av_bytes;i++)
-	{
-		printf("%c \n", read_buf[i]);
-	}
+typedef struct 
+{
+	Date_t initial_date;
+	Date_t last_date;
+}Request_t;
+	
+class LogInterface: public UartCommunication
+{
+	protected:
+		Data_t data_logs_buffer[256] = {0};
+		int n_logs = 0;
+	public:
+		LogInterface(): UartCommunication(BR9600, ONE_STOP_BIT, DISABLE_PARITY_BIT, EIGHT_BITS_PER_BYTE)
+		{
+			printf("Log Interface initialized.\n");
+		} 
+		void getLogData()
+		{
+			int n_received_logs = 0;
+			Data_t data;
+			unsigned char buffer[256];
+			n_received_logs = read_data(buffer)/8;
+			for (int i = 0; i< n_received_logs; i++)
+			{		
+				data.telemetry.temperature = buffer[0 + i * 8];
+				data.telemetry.humidity = buffer[1 + i * 8];
+				data.n_prev = buffer[2 + i * 8];
+				data.n_success = buffer[3 + i * 8];
+				data.uc_ID = buffer[4 + i * 8];
+				data.date.year = buffer[5 + i * 8];
+				data.date.month = buffer[6 + i * 8];
+				data.date.day = buffer[7 + i * 8];
+				
+				data_logs_buffer[n_logs++] = data;
+			}
+		}
+
+		void showLogs()
+		{
+			for (int i = 0; i< n_logs; i++)
+			{
+				printf("Log %i:Year: %c, Month: %c, Day: %c, Temperature: %c, Humidity: %c, N previsions: %c, N success: %c, uc ID: %c. \n", i+1,
+					data_logs_buffer[i].date.year, data_logs_buffer[i].date.month, data_logs_buffer[i].date.day, data_logs_buffer[i].telemetry.temperature,
+					data_logs_buffer[i].telemetry.humidity, data_logs_buffer[i].n_prev, data_logs_buffer[i].n_success, data_logs_buffer[i].uc_ID);
+			}
+		}
+
+		void requestLogs(Request_t request)
+		{
+			unsigned char write_buffer[8];
+
+			write_buffer[0] = request.initial_date.year;
+			write_buffer[1] = request.initial_date.month;
+			write_buffer[2] = request.initial_date.day;
+
+			write_buffer[3] = request.last_date.year;
+			write_buffer[4] = request.last_date.month;
+			write_buffer[5] = request.last_date.day;
+			
+			write_buffer[6] = '0';
+			write_buffer[7] = '0';
+
+			write_data(write_buffer, sizeof(write_buffer));
+		}
+
+};
+
+int main(){	
+	LogInterface interface;
+	
+	Request_t rqst;
+	rqst.initial_date.year = '1';
+	rqst.initial_date.month = '2';
+	rqst.initial_date.day = '3';
+
+	rqst.last_date.year = '4';
+	rqst.last_date.month = '5';
+	rqst.last_date.day = '6';
+
+	interface.requestLogs(rqst);
+
+	interface.getLogData();
+
+	interface.showLogs();
+
 	return 0;
 }
